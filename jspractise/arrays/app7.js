ca07ee33e8cb72d090d006f650ee99a910862ba3
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-09-10 11:31:57 +0400

    some corrections

--- jspractise/arrays/app7.js
+++ jspractise/arrays/app7.js
@@ -3,18 +3,20 @@ function myFunction(arr) {
     return result
 }
 console.log(myFunction([2, 3, 34, 34, 3, 3]));
+
+
 function myFunction2(a, b) {
     let result = a.concat(b);
     return result.sort((a, b) => a - b)
 }
-
 console.log(myFunction2([2, 3, 34, 34, 3, 3], [2, 3, 4, 5, 4, 6,]));
+
+
+//Write a function that takes an array of strings as argument. Return the longest string.
 function myFunction3(a) {
     return a.reduce((a, b) => {
         return (a.length > b.length ? a : b)
     })
 
 }
-//Write a function that takes an array of strings as argument. Return the longest string.
-
 console.log(myFunction3(['help', 'me']))
