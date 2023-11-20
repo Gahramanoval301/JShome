9f467743b7ca49c1a2ca882641dbe5671885b98a
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-20 17:57:41 +0300

    add another practical thing to objects

--- jspractise/practiseObj/secondpart/app1.js
+++ jspractise/practiseObj/secondpart/app1.js
@@ -9,6 +9,7 @@
 let calculator = {
     num1: undefined,
     num2: undefined,
+    show: (num1, num2)=>{console.log(`${num1 + num2}`);},
     read() {
         this.num1 = +prompt('enter the first number: ');
         this.num2 = +prompt('enter the second number: ');
@@ -20,7 +21,13 @@ let calculator = {
         return this.num1 * this.num2
     }
 };
+//TODO Rules for this code:
+// 1. your prop of object if is funtion, then write it as a ARROW FUNCTION
+// 2. if you use the props of object inside your another prop of object, then use this key
+ 
+
 
 calculator.read();
+calculator.show(12, 23);
 alert(calculator.sum());
 alert(calculator.mul());
